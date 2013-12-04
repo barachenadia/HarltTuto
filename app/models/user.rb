bf534ebd848529fcc979fbc3c03411f3de80578e
barachenadia <barache.nadia@yahoo.fr>
2013-12-04 20:26:35 +0100

    Modeling-users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
 	has_many :microposts
-	
+
+before_save { self.email = email.downcase }
+validates :name, :presence => true , :length => {:maximum => 50}
+validates :email, :presence => true 
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+validates :email , :presence => true,  :uniqueness => { :case_sensitive => false } ,:format => {:with => VALID_EMAIL_REGEX } 
+has_secure_password
+validates :password, :length => { :minimum => 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 TutoApp::Application.routes.draw do
+  get "users/new"
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
@@ -7,7 +8,7 @@ TutoApp::Application.routes.draw do
   match '/help',    :to => 'static_pages#help',    :via => 'get'
   match '/about',   :to => 'static_pages#about',   :via => 'get'
   match '/contact', :to =>'static_pages#contact',  :via =>'get'
-  
+  match '/signup',  :to =>'users#new',       :via =>'get'
 
 
   resources :microposts
