8325b57d0567e63f2bb087339450167796c54350
barachenadia <barache.nadia@yahoo.fr>
2013-12-04 13:49:41 +0100

    Filling in the layout, Foundation

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 TutoApp::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
+  get "static_pages/about"
+  get "static_pages/contact"
+  
+  match '/help',    :to => 'static_pages#help',    :via => 'get'
+  match '/about',   :to => 'static_pages#about',   :via => 'get'
+  match '/contact', :to =>'static_pages#contact',  :via =>'get'
+  
+
+
   resources :microposts
 
   resources :users
@@ -9,7 +18,7 @@ TutoApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
