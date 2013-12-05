7ac09fc1a6e7f892b64d54bdd2903caccff6cd28
barachenadia <barache.nadia@yahoo.fr>
2013-12-05 01:08:31 +0100

    Inscription : msg d'erreurs formulaire, formulaire

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Nadia"
+    email    "barache.nadia@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,6 +10,15 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title("Ruby on Rails Tutorial Sample App") }
   end
+
+  describe "profile page" do
+  	let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
+  	
+  end
 end
 
 
