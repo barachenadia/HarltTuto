3b32787e61365602ccfef0038381f03bd6049798
barachenadia <barache.nadia@yahoo.fr>
2013-12-06 00:10:55 +0100

    Edit, update, index and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -6,6 +6,10 @@ module SessionsHelper
     user.update_attribute(:remember_token, User.encrypt(remember_token))
     self.current_user = user
   end
+def current_user?(user)
+    user == current_user
+  end
+
 
 def signed_in?
     !current_user.nil?
@@ -24,6 +28,16 @@ end
 def sign_out
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+
+  def redirect_back_or(default)
+    redirect_to(session[:return_to] || default)
+    session.delete(:return_to)
+  end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
   end
 
 end
